be10c0092d820bbdb6c09c45a9069e4a2e007036
Daughty Sharma <daughtysharma178@gmail.com>
2018-08-04 08:55:07 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -233,11 +233,7 @@
    *
    * Code required to start the app
    *
-   * NOTE: To simplify this codelab, we've used localStorage.
-   *   localStorage is a synchronous API and has serious performance
-   *   implications. It should not be used in production applications!
-   *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
-   *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
+   * 
    ************************************************************************/
 
   
